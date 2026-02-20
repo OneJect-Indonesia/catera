dc30baa8c05400be6650abd1761fa82344233877
farhnalwnd <raja.farhan.alwanda@gmail.com>
2026-02-20 11:14:56 +0700

    firts commit

---
+++ app/Models/Unauthorized.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Unauthorized extends Model
+{
+    /** @use HasFactory<\Database\Factories\UnauthorizedFactory> */
+    use HasFactory;
+}
